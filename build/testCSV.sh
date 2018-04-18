d19ec62daaa923543cc81082b836b4ba17e208de
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-04-18 12:49:46 +0200

    Update testCSV.sh

--- build/testCSV.sh
+++ build/testCSV.sh
@@ -8,7 +8,7 @@
 
 errors=0
 sleep 2
-username=$(curl -s -X GET "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}" | jq -r '.user.login')
+username=$(curl -s -H "Authorization: token ${TOKEN}" -X GET "https://api.github.com/repos/${SEMAPHORE_REPO_SLUG}/pulls/${PULL_REQUEST_NUMBER}" | jq -r '.user.login')
 
 if [ ! -f "$username.csv" ]; then
   echo "File missing. Make sure it has the correct format" "$username.csv"
